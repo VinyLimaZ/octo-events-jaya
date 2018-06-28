987c41124b94243da9f03677c0e370a1aafcbd11
Vinicius Lima <vinylimaz@live.com>
2018-06-28 12:46:15 -0300

    Add all routes asked

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@
 
 Rails.application.routes.draw do
   post '/', to: 'issues#create'
+  resources :issues, only: [] do
+    get 'events', to: 'events#index', on: :member
+    get 'statistics', to: 'event#statistcs', on: :collection
+  end
 end
