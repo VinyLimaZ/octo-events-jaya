140b94e59bbdf4cf985c862821d62a76d136b84d
Vinicius Lima <vinylimaz@live.com>
2018-06-28 03:25:08 -0300

    Add method to create event based on state param

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,13 +2,18 @@ class IssuesController < ApplicationController
   before_action :verify_github_webhook_token, only: :create
 
   def create
-    Issue.create_or_update(issue_params)
+    Issue.find_or_create_by(issue_params)
     render json: { message: :ok }, status: :created
   end
 
   private
 
   def issue_params
-    params.require(:issue).permit(:number, :title, :body, :state, :url)
+    params.require(:issue).permit(
+      :number,
+      :title,
+      :body,
+      :state,
+      :url)
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,9 @@
 class Issue < ApplicationRecord
+  has_many :events
+
+  validates_uniqueness_of :number
+
+  def state=(event_class = Event, state)
+    event_class.create(action: state, issue: self)
+  end
 end
