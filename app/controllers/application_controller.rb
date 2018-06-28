7c7c4e5b2225323ef71853d646ff5180a0204759
Vinicius Lima <vinylimaz@live.com>
2018-06-28 16:26:33 -0300

    Add not_found_exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::API
   include ExceptionHelper
 
   rescue_from SecurityError, with: :signature_mismatch!
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_exception!
 end
--- app/controllers/concerns/exception_helper.rb
+++ app/controllers/concerns/exception_helper.rb
@@ -1,6 +1,12 @@
 module ExceptionHelper
 
   def signature_mismatch!
-    render json: { errors: 'Signature mismatch!' }, status: :unauthorized
+    render json: {
+      errors: I18n.t('errors.messages.signature_mismatch')
+    }, status: :unauthorized
+  end
+
+  def not_found_exception!
+    render json: { errors: I18n.t('errors.messages.not_found')}, status: :not_found
   end
 end
