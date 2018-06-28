143a9e035271b7d57c0f5a8ad223c8849dd76b17
Vinicius Lima <vinylimaz@live.com>
2018-06-28 15:35:20 -0300

    Add events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628022139) do
+ActiveRecord::Schema.define(version: 20180628041201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "action"
+    t.bigint "issue_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issue_id"], name: "index_events_on_issue_id"
+  end
 
   create_table "issues", force: :cascade do |t|
     t.string "url"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 20180628022139) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "issues"
 end
